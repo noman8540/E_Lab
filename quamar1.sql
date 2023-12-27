735f126301af721cbae58d1cefd2e70f6e90c3b3
MD NOMAN QUAMAR <77150066+noman8540@users.noreply.github.com>
2023-12-28 02:25:55 +0530

    Add files via upload

---
+++ quamar1.sql
@@ -0,0 +1,23 @@
+create database quamar1;
+
+use quamar1;
+
+select * from register;
+
+select * from alltest;
+drop table orderdetails;
+
+show tables;
+
+select * from orderdetails;
+
+select * from orderdetails;
+alter table orderdetails drop alltest_id;
+
+select * from alltest;
+
+drop table orderdetails;
+delete from cart where id=153;
+
+delete from alltest where id=105;
+
